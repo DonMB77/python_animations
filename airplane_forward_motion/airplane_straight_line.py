8acfe0dac94435d115eadc62578b8d3d82c0a4e7
DonMB77 <purposefuldrifter@gmail.com>
2026-01-10 17:37:52 +0100

    airplane, building model + set_data functionality

--- airplane_forward_motion/airplane_straight_line.py
+++ airplane_forward_motion/airplane_straight_line.py
@@ -24,7 +24,12 @@ frame_amount=len(t)
 
 def update_plot(num):
     plane_trajectory.set_data(x[0:num], y[0:num])
-    return plane_trajectory,
+    plane_1.set_data([x[num]-40,x[num]+20], [y[num],y[num]])
+    plane_2.set_data([x[num]-20,x[num]], [y[num]+0.3,y[num]])
+    plane_3.set_data([x[num]-20,x[num]], [y[num]-0.3,y[num]])
+    plane_4.set_data([x[num]-40,x[num]-30], [y[num]+0.15,y[num]])
+    plane_5.set_data([x[num]-40,x[num]-30], [y[num]-0.15,y[num]])
+    return plane_trajectory,plane_1,plane_2,plane_3,plane_4,plane_5
     
 fig = plt.figure(figsize=(16,9), dpi=120, facecolor=(0.8,0.8,0.8))
 gs = gridspec.GridSpec(2,2)
@@ -32,6 +37,19 @@ gs = gridspec.GridSpec(2,2)
 # Subplot 1
 ax0 = fig.add_subplot(gs[0,:], facecolor=(0.9,0.9,0.9))
 plane_trajectory, = ax0.plot([],[],'g',linewidth=2)
+plane_1, = ax0.plot([],[],'k',linewidth=10)
+plane_2, = ax0.plot([],[],'k',linewidth=5)
+plane_3, = ax0.plot([],[],'k',linewidth=5)
+plane_4, = ax0.plot([],[],'k',linewidth=3)
+plane_5, = ax0.plot([],[],'k',linewidth=3)
+
+# Draw houses
+house_1 = ax0.plot([100,100],[0,1.0],'k',linewidth=7)
+house_2 = ax0.plot([300,300],[0,1.0],'k',linewidth=7)
+house_3 = ax0.plot([700,700],[0,0.7],'k',linewidth=15)
+house_4 = ax0.plot([900,900],[0,0.9],'k',linewidth=10)
+house_5 = ax0.plot([1300,1300],[0,1.0],'k',linewidth=20)
+
 plt.xlim(x[0], x[-1])
 plt.ylim(0, y[0]+1)
 
