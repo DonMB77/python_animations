1477cab07634624cec3c94e2260775338550609f
DonMB77 <purposefuldrifter@gmail.com>
2026-01-11 16:20:47 +0100

    labeling, grid and stopwatch

--- airplane_forward_motion/airplane_straight_line.py
+++ airplane_forward_motion/airplane_straight_line.py
@@ -29,14 +29,19 @@ def update_plot(num):
     plane_3.set_data([x[num]-20,x[num]], [y[num]-0.3,y[num]])
     plane_4.set_data([x[num]-40,x[num]-30], [y[num]+0.15,y[num]])
     plane_5.set_data([x[num]-40,x[num]-30], [y[num]-0.15,y[num]])
-    return plane_trajectory,plane_1,plane_2,plane_3,plane_4,plane_5
+    
+    stopwatch0.set_text(str(round(t[num],1))+' hrs')
+    distance_counter0.set_text(str(int(x[num]))+' km')
+
+    return plane_trajectory,plane_1,plane_2,plane_3,plane_4,plane_5,stopwatch0,\
+    distance_counter0
     
 fig = plt.figure(figsize=(16,9), dpi=120, facecolor=(0.8,0.8,0.8))
 gs = gridspec.GridSpec(2,2)
 
 # Subplot 1
 ax0 = fig.add_subplot(gs[0,:], facecolor=(0.9,0.9,0.9))
-plane_trajectory, = ax0.plot([],[],'g',linewidth=2)
+plane_trajectory, = ax0.plot([],[],'r:o',linewidth=2)
 plane_1, = ax0.plot([],[],'k',linewidth=10)
 plane_2, = ax0.plot([],[],'k',linewidth=5)
 plane_3, = ax0.plot([],[],'k',linewidth=5)
@@ -50,10 +55,20 @@ house_3 = ax0.plot([700,700],[0,0.7],'k',linewidth=15)
 house_4 = ax0.plot([900,900],[0,0.9],'k',linewidth=10)
 house_5 = ax0.plot([1300,1300],[0,1.0],'k',linewidth=20)
 
+box_obj = dict(boxstyle="square",fc=(0.9,0.9,0.9),ec="r",lw=1)
+stopwatch0=ax0.text(1400,0.65,"",size=20,color="green",bbox=box_obj)
+
+box_obj2 = dict(boxstyle="square",fc=(0.9,0.9,0.9),ec="g",lw=1)
+distance_counter0 = ax0.text(1000,0.5,"",size=20,color="r",bbox=box_obj2)
+
 plt.xlim(x[0], x[-1])
 plt.ylim(0, y[0]+1)
 plt.xticks(np.arange(x[0],x[-1]+1,x[-1]/4),size=15)
 plt.yticks(np.arange(0,y[-1]+2,y[-1]/y[-1]),size=15)
+plt.xlabel("x-distance",fontsize=15)
+plt.ylabel("y-distance",fontsize=15)
+plt.title("Airplane",fontsize=20)
+plt.grid(True)
 
 plane_ani = animation.FuncAnimation(fig, update_plot, frames=frame_amount, interval=20, blit=True)
 plt.show()
